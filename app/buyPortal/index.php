d468a0aa70406709f3f1b7b31112fc20d8e6d784
Max van der Velde <maxvelde_3010@hotmail.com>
2020-01-13 21:39:28 +0100

    Toevoegingen voor demo

--- app/buyPortal/index.php
+++ app/buyPortal/index.php
@@ -1,58 +1,86 @@
 <?php include "../../meta.php";?>
 <title>temp title</title>
-<link rel="stylesheet"  type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
 
-  <main>
-    <div id="ItemLijstID">
-      <div class="ItemsHead">
-         <h2>Items</h2>
-         <form id="searchFormID">
-           <input type="text" name="search" placeholder="Zoeken" id="searchInputID">
-           <button type="submit" name="submit" id="searchButtonID">
+    <main>
+        <div id="ItemLijstID">
+            <div class="ItemsHead">
+                <h2>Items</h2>
+                <form id="searchFormID">
+                    <input type="text" name="search" placeholder="Zoeken" id="searchInputID">
+                    <button type="submit" name="submit" id="searchButtonID">
 
-          </button>
-         </form>
-      </div>
-      <div id="ItemTableContain">
-        <table id="ItemViewTableID">
+                    </button>
+                </form>
+            </div>
+            <div id="ItemTableContain">
+                <table id="ItemViewTableID">
 
-          <tbody>
-            
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <div id="ItemFormID">
-      <div class="ItemHead">
-        <h2>Item toevoegen</h2>
-      </div>
-      <div id="ItemFormContainID">
-        <form method="post">
-          <table id="InputFormID">
-            <tr>
-              <td>Item:</td>
-              <td><input type="text" name="ItemNr"></td>
-            </tr>
-            <tr>
-              <td>Prijs:</td>
-              <td>
-                <select name="Prijs">
-                  <?php
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div id="ItemFormID">
+            <div class="ItemHead">
+                <h2>Item toevoegen</h2>
+            </div>
+            <div id="ItemFormContainID">
+                <form method="post">
+                    <table id="InputFormID">
+                        <tr>
+                            <td>Item:</td>
+                            <td><input type="text" name="ItemNr"></td>
+                        </tr>
+                        <tr>
+                            <td>Prijs:</td>
+                            <td>
+                                <select name="Prijs">
+                                    <?php
                       echo "Prijs <br />";
                   ?>
-                </select>
-              </td>
-            </tr>
+                                </select>
+                            </td>
+                        </tr>
 
-          </table>
-        </form>
-        <p id="noticeParID">Als het Itemnummer al bestaat in de database zal
-          deze worden geüpdate met de nieuwe waarden of worden verwijderd.</p>
-      </div>
-    </div>
-  </main>
+                    </table>
+                </form>
+                <p id="noticeParID">Als het Itemnummer al bestaat in de database zal
+                    deze worden geüpdate met de nieuwe waarden of worden verwijderd.</p>
+            </div>
+        </div>
+    </main>
+
+    <main>
+        <table>
+            <tr>
+                <td>
+                    Pennen €3,50 BIC
+                </td>
+                <td>
+                    Printer inkt €21,30 CANNON
+                </td>
+                <td>
+                    A4 papier 100 stuks €10,00 NICEDAY
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Kleur printer inkt €30,30 CANNON
+                </td>
+                <td>
+                    Potloden €1,50 BIC
+                </td>
+                <td>
+                    Sticky notes geel €4,50 NICEDAY
+                </td>
+            </tr>
+        </table>
+    </main>
 </body>
+
 </html>
--- app/depManPortal/index.php
+++ app/depManPortal/index.php
@@ -1,13 +1,47 @@
 <?php include "../../meta.php";?>
 <title>temp title</title>
-<link rel="stylesheet"  type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
 
-  <main>
-  
-  </main>
+    <main>
+        <table>
+            <thead>
+                <th>Naam</th>
+                <th>Product</th>
+                <th>Leverancier</th>
+                <th>Prijs</th>
+                <th>Reden</th>
+                <th>Actie</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Rien</td>
+                    <td>Pennen</td>
+                    <td>BIC</td>
+                    <td>€3,50</td>
+                    <td>Mijn pennen zijn allemaal leeg.</td>
+                    <td>
+                        <button>Goedkeuren</button>
+                        <button>Afkeuren</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Piet</td>
+                    <td>Printer inkt</td>
+                    <td>CANNON</td>
+                    <td>€21,30</td>
+                    <td>Ik vind de tint inkt niet zo mooi die we nu hebben.</td>
+                    <td>
+                        <button>Goedkeuren</button>
+                        <button>Afkeuren</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
 
 </body>
+
 </html>
