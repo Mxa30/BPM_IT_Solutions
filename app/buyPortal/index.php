7b4d9cc6b07ccac31964e86b57d86faa22c7ef06
Arnotje <55438056+Arnotje@users.noreply.github.com>
2020-01-13 16:54:03 +0100

    Update index.php

--- app/buyPortal/index.php
+++ app/buyPortal/index.php
@@ -8,25 +8,53 @@
   <main>
     <div id="ItemLijstID">
       <div class="ItemsHead">
-         <h2>Items</h2>
+         <h2>Item zoeken</h2>
          <form id="searchFormID">
            <input type="text" name="search" placeholder="Zoeken" id="searchInputID">
-           <button type="submit" name="submit" id="searchButtonID">
+           <button type="submit" name="submit" id="searchButtonID"> Zoeken
+
 
           </button>
+
          </form>
       </div>
       <div id="ItemTableContain">
         <table id="ItemViewTableID">
 
           <tbody>
-            
+
           </tbody>
         </table>
       </div>
     </div>
     <div id="ItemFormID">
       <div class="ItemHead">
+        <h2> Items </h2>
+        <div class="Itemlist">
+          <p> Hier komen de Items te staan. </p>
+          <table>
+            <tr>
+              <td>Potlood </td>
+              <td>€1,- </td>
+            </tr>
+            <tr>
+              <td>pen </td>
+              <td>€1,50 </td>
+            </tr>
+            <tr>
+              <td>Printer </td>
+              <td>€75,00 </td>
+            </tr>
+            <tr>
+              <td>Scanner </td>
+              <td>€70,- </td>
+            </tr>
+            <tr>
+              <td>Laptop </td>
+              <td>€150,- </td>
+            </tr>
+          </table>
+        </div>
         <h2>Item toevoegen</h2>
       </div>
       <div id="ItemFormContainID">
