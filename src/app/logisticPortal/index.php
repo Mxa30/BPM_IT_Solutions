bffef838a98ef0bffe99ddfecf38cebb36616cbc
Max van der Velde <maxvelde_3010@hotmail.com>
2020-01-22 13:22:37 +0100

    Edits

--- src/app/logisticPortal/index.php
+++ src/app/logisticPortal/index.php
@@ -13,7 +13,24 @@ include "functions.php";
       echo ("<link rel='stylesheet' href='" . APP_PATH . "/header/style.css". "'>");
     ?>
     <main>
-
+      <table>
+        <thead>
+          <td>Order ID</td>
+          <td>Naam</td>
+          <td>Department</td>
+          <td>Product</td>
+          <td>Aantal</td>
+          <td>Actie</td>
+        </thead>
+        <tbody>
+          <td>1</td>
+          <td>Max van der Velde</td>
+          <td>Logistics</td>
+          <td>Laptop</td>
+          <td>3</td>
+          <td>asd</td>
+        </tbody>
+      </table>
     </main>
 </body>
 
